c7a29cb7dec9cbdffc5ac81b31520196a6d2cf24
Victor Lourenço <victorhlo12@hotmail.com>
2023-08-07 15:17:38 -0300

    FEAT: Implementando o Department

--- SalesMVC/Controllers/HomeController.cs
+++ SalesMVC/Controllers/HomeController.cs
@@ -1,5 +1,5 @@
 ﻿using Microsoft.AspNetCore.Mvc;
-using SalesMVC.Models;
+using SalesMVC.Models.ViewModels;
 using System.Diagnostics;
 
 namespace SalesMVC.Controllers
---
+++ SalesMVC/Models/ViewModels/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace SalesMVC.Models.ViewModels
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
